679458eee612c3533e8908fb65c79dc155c586da
guenanank <guenanank@yahoo.com>
2016-10-26 13:53:50 +0700

    setup

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,9 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        
+        // appending custom middleware 
+        'GMC\Http\Middleware\HttpsProtocol'
     ];
 
     /**
