65370ce408d70f7917173c78bcb81fbf3e1d6015
guenanank <guenanank@yahoo.com>
2016-11-17 15:17:25 +0700

    setup

--- app/Http/Middleware/WithAPI.php
+++ app/Http/Middleware/WithAPI.php
@@ -5,8 +5,8 @@ namespace GMC\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
-class WithAPI
-{
+class WithAPI {
+
     /**
      * Handle an incoming request.
      *
@@ -14,18 +14,18 @@ class WithAPI
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
+    public function handle($request, Closure $next) {
         $username = Auth::user()->username;
         $apiToken = \Illuminate\Support\Facades\Crypt::encrypt($username);
-        $getEmployee = file_get_contents('https://api.gramedia-majalah.com/v1/gateway/employee/' . $username . '?token=' . $apiToken);
+
+        $getEmployee = file_get_contents('https://api.gramedia-majalah.com/v1/gateway/employee/' . $username . '?token=' . $apiToken, false, $context);
         //$getEmployee = file_get_contents('http://localhost/api/public/v1/gateway/employee/' . $username . '?token=' . $apiToken);
         $employee = collect(json_decode($getEmployee));
-        if($employee->isEmpty() == false) :
+        if ($employee->isEmpty() == false) :
             $request->session()->put('api_token', $apiToken);
             $request->session()->put('employee', $employee);
             return $next($request);
         endif;
-        
     }
+
 }
