b6ea418d7b7291cdf772e9cf5032d2ad362712c1
guenanank <guenanank@yahoo.com>
2016-11-17 14:46:46 +0700

    setup

--- app/Http/Middleware/WithAPI.php
+++ app/Http/Middleware/WithAPI.php
@@ -18,7 +18,8 @@ class WithAPI
     {
         $username = Auth::user()->username;
         $apiToken = \Illuminate\Support\Facades\Crypt::encrypt($username);
-        $getEmployee = file_get_contents('https://api.gramedia-majalah.com/v1/gateway/employee/' . $request->username . '?token=' . $apiToken);
+        $getEmployee = file_get_contents('https://api.gramedia-majalah.com/v1/gateway/employee/' . $username . '?token=' . $apiToken);
+        //$getEmployee = file_get_contents('http://localhost/api/public/v1/gateway/employee/' . $username . '?token=' . $apiToken);
         $employee = collect(json_decode($getEmployee));
         if($employee->isEmpty() == false) :
             $request->session()->put('api_token', $apiToken);
